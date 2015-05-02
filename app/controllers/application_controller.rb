d92226ba7486cd524da9b4cf6de5264ae8355bf8
Shunichi Ikegami <sike.tm@gmail.com>
2015-05-02 12:25:10 +0900

    以前練習で書いたところまで

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include SessionMethods
+  include MobileCheckable
 
   before_action :authenticate_user!
+  before_action :check_for_mobile
 end
